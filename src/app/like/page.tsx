b8e280a2be2c678377ff07fe141fb83d6c6a350b
Om Patil <patil4om@gmail.com>
2025-12-16 18:49:46 +0530

    added Empty Likes section

--- src/app/like/page.tsx
+++ src/app/like/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import EmptyLikeList from '@/components/EmptyLikeList/EmptyLikeList'
 import Card from '@/components/FoodCards/Card'
 import { useAppSelector } from '@/lib/store/store'
 import React from 'react'
@@ -8,12 +9,16 @@ const page = () => {
     const likedItems=useAppSelector((state)=>state.likes.items)
   return (
     <>
-        
-        <div className='flex items-center flex-wrap pt-30 gap-10 mx-22'>
+
+      {
+        (likedItems.length!==0)? (<div className='flex items-center flex-wrap pt-30 gap-10 mx-22'>
             {likedItems.map(obj=>{
                 return <Card key={obj.id} recipe={obj}/>
             })}
-        </div>
+        </div>):(<EmptyLikeList/>)
+      }
+        
+        
     
     
     
--- src/components/FoodCards/FoodCards.tsx
+++ src/components/FoodCards/FoodCards.tsx
@@ -4,7 +4,7 @@ import Card from './Card';
 import { recipeArr } from '@/types/RecipeTypes/Recipe';
 
 export default async function  FoodCards() {
-    const response=await fetch("https://dummyjson.com/recipes?limit=100")
+    const response=await fetch("https://dummyjson.com/recipes?limit=100", { cache: "force-cache" })
     let recipes=await response.json();
     const data:recipeArr=recipes.recipes;
     // console.log(data);
