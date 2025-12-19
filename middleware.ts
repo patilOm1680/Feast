9866b4473cc3c8b13af30dec30af3f7376a52e16
Om Patil <patil4om@gmail.com>
2025-12-19 12:37:07 +0530

    Protected the routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/product/:id','/'])
+const isProtectedRoute = createRouteMatcher(['/product/:id'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
